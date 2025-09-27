b60fa70bc4d588f3d62847bf3d1688df2ad6e7ac
66011212243 <66011212243@msu.ac.th>
2025-09-27 21:54:48 +0700

    อัปเดต dependencies

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express')
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const bcrypt = require("bcryptjs");
+require('dotenv').config();
 const app = express();
 app.use(express.json());
+
 
 const PORT = process.env.PORT || 3000;
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'lotto888_db',
-    
-    password: 'lotto888',
-    database: 'lotto888_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 
